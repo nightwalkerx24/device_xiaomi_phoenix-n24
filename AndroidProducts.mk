1b9476cebdda42f28c9cf899b4891ed8bc08c642
nightwalkerx24 <augustin85vayyattupuzha@gmail.com>
2021-07-30 05:17:42 +0000

    init Bliss

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_phoenix.mk
+    $(LOCAL_DIR)/bliss_phoenix.mk
+
+COMMON_LUNCH_CHOICES := \
+    bliss_phoenix-user \
+    bliss_phoenix-userdebug \
+    bliss_phoenix-eng
